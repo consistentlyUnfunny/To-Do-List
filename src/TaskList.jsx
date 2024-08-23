aaf8abdf871fd261e0a5e165dc18efd8d9d73eb7
KH <khdotxii@gmail.com>
2024-08-23 12:08:47 +0800

    added checkbox

--- src/TaskList.jsx
+++ src/TaskList.jsx
@@ -1,10 +1,33 @@
+import React, { useState } from 'react';
+
 function TaskList({task, deleteTask}){
+    const [checkedState, setCheckedState] = useState(
+        new Array(task.length).fill(false)
+      );
+    
+      const handleCheckboxChange = (index) => {
+        const updatedCheckedState = checkedState.map((item, i) =>
+          i === index ? !item : item
+        );
+        setCheckedState(updatedCheckedState);
+    
+        // Delay deletion by 0.5 seconds
+        setTimeout(() => {
+          deleteTask(index);
+        }, 500);
+      };
     return (
         <div className = 'task-container'>
             <ul>
                 {task.map((task, index) => {
                     return <>
                         <li className = "task-item" key = {index}>
+                            <label className="custom-checkbox">
+                                <input type="checkbox" 
+                                className="task-checkbox" 
+                                onChange = {() => handleCheckboxChange(index)}/>
+                                <span className="checkmark"></span>
+                            </label>
                             {task}
                             <span className="material-symbols-outlined" 
                             id = "close-button"
